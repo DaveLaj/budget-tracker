14a24e1c6bbf730a3b54ec7059b18c202180dd42
unknown <lapojapod@gmail.com>
2024-05-28 17:37:16 +0800

    dabbled with http server

--- main.go
+++ main.go
@@ -4,14 +4,22 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	// "fmt"
-
+	"fmt"
+	"log"
+	"net/http"
 	// "github.com/DaveLaj/budget-tracker/database"
-
-	"github.com/DaveLaj/budget-tracker/cmd"
+	// "github.com/DaveLaj/budget-tracker/cmd"
 	// "github.com/DaveLaj/budget-tracker/utils"
 )
 
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
 func main() {
-	cmd.Execute() // command used to enable custom commands
+	// cmd.Execute() // command used to enable custom commands
+
+	http.HandleFunc("/hello", hello)
+
+	log.Fatal(http.ListenAndServe("localhost:4200", nil))
 }
