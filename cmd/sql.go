d71b44c20b66cdf8fe2017636a66b0675fc25e50
unknown <lapojapod@gmail.com>
2024-05-28 14:51:25 +0800

    toFix: sql command to run query

--- cmd/sql.go
+++ cmd/sql.go
@@ -4,8 +4,11 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	_ "database/sql"
 	"fmt"
 
+	"github.com/DaveLaj/budget-tracker/database"
+	"github.com/DaveLaj/budget-tracker/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +19,32 @@ var sqlCmd = &cobra.Command{
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sql called")
+		fmt.Println("Ongoing work on sql command.")
+
+		// Setup for the LoadConfig function
+		cfg, err := utils.LoadConfig()
+		if err != nil {
+			fmt.Println("Error loading config")
+			return
+		}
+		db, err := database.CreateDBConnection(cfg, "budget")
+		if err != nil {
+			fmt.Println("Error connecting to database")
+		}
+		defer db.Close()
+
+		// This is where the code for the sql command will go
+
+		table, err := db.Query("CREATE TABLE IF NOT EXISTS expenses (id SERIAL PRIMARY KEY, name TEXT, amount FLOAT, date DATE);")
+		if err != nil {
+			fmt.Println("Error creating table")
+		} else {
+			fmt.Println("Table created successfully")
+		}
+		defer table.Close()
+
+		// build := exec.Command("go", "build", "-o", "sql", "sql.go")
+		// build.run() // run the build command
 	},
 }
 
--- main.go
+++ main.go
@@ -4,24 +4,25 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
-	"github.com/DaveLaj/budget-tracker/database"
-	// "github.com/DaveLaj/budget-tracker/cmd"
+	// "github.com/DaveLaj/budget-tracker/database"
+	"github.com/DaveLaj/budget-tracker/cmd"
+	// "github.com/DaveLaj/budget-tracker/utils"
 )
 
 func main() {
-	// cmd.Execute() // command used to enable custom commands
-	cfg, err := LoadConfig()
-	if err != nil {
-		fmt.Println("Error loading config")
-		return
-	}
-	db, err := database.CreateDBConnection(cfg, "budget")
-	if err != nil {
-		fmt.Println("Error connecting to database")
-		return
-	}
-	defer db.Close()
+	cmd.Execute() // command used to enable custom commands
+	// cfg, err := utils.LoadConfig()
+	// if err != nil {
+	// 	fmt.Println("Error loading config")
+	// 	return
+	// }
+	// db, err := database.CreateDBConnection(cfg, "budget")
+	// if err != nil {
+	// 	fmt.Println("Error connecting to database")
+	// 	return
+	// }
+	// defer db.Close()
 
 }
